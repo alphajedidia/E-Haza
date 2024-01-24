097c100e61dcdfc97dd48b44d6c4fbd1ddc167d5
alphaJedidia <alphajedidia01@gmail.com>
2024-01-24 21:46:54 +0000

    add: loging page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
+import Login from "./pages/login"
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1 className=' text-center text-red-800 text-5xl'>Test</h1>
-    </>
-  )
+  return (<>
+    <Login/>
+  </>)
 }
 
 export default App
