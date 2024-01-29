74e1aa19adf090b1add05fa64ac1cec80d2aba33
Emii-lia <fiaromiangaly@gmail.com>
2024-01-29 13:31:33 +0300

    add: add post create page and its components

---
+++ src/pages/create_post.page.tsx
@@ -0,0 +1,9 @@
+import { CreatePostForm } from "../components/form/create_post.form"
+
+export const CreatePost = ()=>{
+    return(
+        <div className="">
+            <CreatePostForm/>
+        </div>
+    )
+}
