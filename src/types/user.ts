5a9798a617e4867668b7986dc0f2a911726b2b21
Emii-lia <fiaromiangaly@gmail.com>
2024-01-29 13:43:58 +0300

    add: add post and user types

---
+++ src/types/user.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    user_id: string,
+    username: string,
+    profileurl: string,
+    email: string,
+    dob: string,
+    createAt:string,
+    gender:"male"|"female"|"rather not say"
+}
