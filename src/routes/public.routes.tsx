d7856d41694481137a4fbcd1d2cb5d038011c2b9
alphaJedidia <alphajedidia01@gmail.com>
2024-02-02 17:54:42 +0000

    update : public Routes (add : login and signup route)

--- src/routes/public.routes.tsx
+++ src/routes/public.routes.tsx
@@ -1,3 +1,23 @@
 import { RouteObject } from "react-router-dom";
+import LoginLayout from "../components/layout/login.layout";
+import SignUpLayout from "../components/layout/signUp.layout";
 
-export const publicRoutes: RouteObject[] = []
+
+
+export const publicRoutes: RouteObject[] = [
+
+    {
+        path:"auth/*",
+        children:[
+            {
+                path:"login/*",
+                element: <LoginLayout/>,
+            },
+            {
+                path:"signup/*",
+                element: <SignUpLayout/>
+            },
+   
+        ]
+    },
+]
